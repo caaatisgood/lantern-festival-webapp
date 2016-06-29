1cdddd5990a1c6534d8d30acb57ebfca528df729
imrobinized <almigod0718@gmail.com>
2016-06-30 01:04:52 +0800

    api updates

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,26 @@ var request = require('request');
 
 module.exports = function (app) {
 
-  app.get('/api/tag' , function (req, res) {
+  app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
+  app.get('/api/tag', function (req, res) {
     getByTag(function(data) {
       res.json(data);
     })
   });
 
-  app.get('/api/location' , function (req, res) {
+  app.get('/api/location', function (req, res) {
     getByLocation(function(data) {
+      res.json(data);
+    })
+  });
+
+  app.get('/api/mission2', function (req, res) {
+    getByTagMission2(function(data) {
       res.json(data);
     })
   });
@@ -37,6 +49,16 @@ function getByLocation(callback) {
   var location_id = '214902213';
   var locationUrl = 'https://api.instagram.com/v1/locations/' + location_id + '/media/recent?access_token=' + ACCESS_TOKEN;
   request(locationUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(JSON.parse(body));
+    }
+  });
+}
+
+function getByTagMission2(callback) {
+  var tag = '2017lfmission2';
+  var tagRecentUrl = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?access_token=' + ACCESS_TOKEN;
+  request(tagRecentUrl, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       callback(JSON.parse(body));
     }
